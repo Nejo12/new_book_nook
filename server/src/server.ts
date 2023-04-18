25ccc6d0092c9628b5a79fd147b111d7be93a3c8
Gabriel Olaniyi Aborisade <42632188+Nejo12@users.noreply.github.com>
2023-04-19 01:36:45 +0200

    initial server creation

---
+++ server/src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+const app = express();
+const port = 5001;
+
+app.get('/', (req, res) => {
+  res.send('Testing Hello World');
+});
+
+app.listen(port, () => {
+  console.log('Server is running on port: ' + port);
+});
