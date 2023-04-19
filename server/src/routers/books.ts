4eddaa7e2a4188e76f76a638dec0d2d67e739343
Gabriel Olaniyi Aborisade <42632188+Nejo12@users.noreply.github.com>
2023-04-19 17:24:36 +0200

    complete CRUD on book server
    - renamed services to depict functionality

--- server/src/routers/books.ts
+++ server/src/routers/books.ts
@@ -12,11 +12,21 @@ router.get('/', book.allBooks);
 // @route       POST api/books
 // @description add/create/save book
 // @access      will be Private
-router.post(
-  '/',
-  // auth,
-  // roles.grantAccess('createOwn', 'profile'),
-  book.createBook,
-);
+router.post('/', book.createBook);
+
+// @route       GET api/books/:id
+// @description get single book by id
+// @access      Public
+router.get('/:bookId', book.getBook);
+
+// @route       PUT api/books/:id
+// @description edit specific book by its Id
+// @access      will be Private
+router.put('/:bookId', book.updateBook);
+
+// @route       DELETE api/books/:id
+// @description delete book by id
+// @access      will be Private
+router.delete('/:bookId', book.deleteBook);
 
 export default router;
--- server/src/services/book.ts
+++ server/src/services/book.ts
@@ -2,12 +2,58 @@ import Book from '../models/Book';
 
 import { BookDocument } from '../types/types';
 
-function findAll(): Promise<BookDocument[]> {
+function findAllBooks(): Promise<BookDocument[]> {
   return Book.find().sort({ title: 1 }).exec();
 }
 
-function create(book: BookDocument): Promise<BookDocument> {
+function createBook(book: BookDocument): Promise<BookDocument> {
   return book.save();
 }
 
-export default { findAll, create };
+function findBookById(bookId: string): Promise<BookDocument | null> {
+  return Book.findById(bookId).exec();
+}
+
+async function updateBook(
+  bookId: string,
+  update: Partial<BookDocument>,
+): Promise<BookDocument> {
+  const book = await Book.findById(bookId).exec();
+  if (!book) {
+    throw new Error(`Book ${bookId} Not Found`);
+  }
+  if (update.title) {
+    book.title = update.title;
+  }
+  if (update.isbn) {
+    book.isbn = update.isbn;
+  }
+  if (update.author) {
+    book.author = update.author;
+  }
+  if (update.copies) {
+    book.copies = update.copies;
+  }
+  if (update.description) {
+    book.description = update.description;
+  }
+  if (update.publishedDate) {
+    book.publishedDate = update.publishedDate;
+  }
+  if (update.publisher) {
+    book.publisher = update.publisher;
+  }
+  return await book.save();
+}
+
+function deleteBook(bookId: string): Promise<BookDocument | null> {
+  return Book.findByIdAndDelete(bookId).exec();
+}
+
+export default {
+  findAllBooks,
+  createBook,
+  findBookById,
+  updateBook,
+  deleteBook,
+};
