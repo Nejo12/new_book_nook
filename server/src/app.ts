cdbe7e4d7aa60c68001ddf4be5d4c478cdad82c0
Gabriel Olaniyi Aborisade <42632188+Nejo12@users.noreply.github.com>
2023-04-25 22:22:24 +0200

    port 3000 for Adaptable deployment

--- server/src/app.ts
+++ server/src/app.ts
@@ -26,7 +26,7 @@ app.use(
 
 connectDB();
 
-app.set('port', env.PORT);
+app.set('port', env.PORT || process.env.PORT || 3000); // as suggested by adaptative deployment.
 
 // to enable retrieval and send ability of json
 app.use(express.json());
